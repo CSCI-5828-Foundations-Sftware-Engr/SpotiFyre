e4e4ffdf71e668a242902f15476a353affe2117f
Utsav Gupta <22051046+MrUtsavG@users.noreply.github.com>
2023-05-04 17:34:12 -0600

    added add track functionality

--- services/playlist/utils.py
+++ services/playlist/utils.py
@@ -38,7 +38,7 @@ def get_group_members(group):
     return members
 
 
-def get_all_tracks(members):
+def get_all_tracks(members, num_tracks):
     # Get list of user_ids of all group members
     if members:
         member_ids = [member.user_id for member in members]
@@ -46,25 +46,32 @@ def get_all_tracks(members):
     # Get all distinct tracks for all member ids
         all_tracks = session.query(UserTracks.user_id, UserTracks.track_id) \
                         .join(Tracks, UserTracks.track_id == Tracks.id) \
-                        .filter(UserTracks.user_id.in_(member_ids)) \
-                        .group_by(UserTracks.user_id)
+                        .filter(UserTracks.user_id.in_(member_ids)).all()
+                        # .group_by(UserTracks.user_id)
+        
+        track_ids = set()
+
+        for track in all_tracks:
+            track_ids.add(track.track_id)
         
         # Create dict of user to list of tracks
-        # user_tracks = {}
+        user_tracks = list(track_ids)
 
-        return all_tracks
+        pl_tracks = session.query(Tracks).filter(Tracks.id.in_(user_tracks)).all()
+
+        return pl_tracks
     else:
         print("No group found")
 
 
-# def get_playlist_tracks(all_tracks, num_tracks):
+def get_playlist_tracks(pl_tracks, num_tracks):
 
-#     pl_tracks = dict() # [track for track in all_tracks]
+    pl_track_uris = list() # [track for track in all_tracks]
     
-#     for i in range(num_tracks):
-#         pl_tracks.append(all_tracks)
+    for track in pl_tracks:
+        pl_track_uris.append(track.track_uri)
     
-#     return pl_tracks
+    return pl_track_uris
 
 def create_spotify_playlist(user_id, playlist_id, name):
     user = session.query(User).filter_by(id=user_id).first()
@@ -86,11 +93,19 @@ def create_spotify_playlist(user_id, playlist_id, name):
     return new_playlist
 
 
-# def add_tracks(tracks, playlist):
-#     pl_id = playlist["id"]
+def add_tracks(user_id, tracks, playlist):
+    user = session.query(User).filter_by(id=user_id).first()
+    cache_path = f"cache_{user.id}"
+    f = open(cache_path, "w")
+    f.write(user.cache)
+    f.close()
 
-#     print("pl_id", pl_id)
+    sp = create_spotify_instance(cache_path)
+    spotify_user_id = sp.current_user()['id']
 
+    add_tracks_req = sp.user_playlist_add_tracks(user=spotify_user_id, playlist_id=playlist['id'], tracks=tracks)
+
+    return add_tracks_req
 
 
 def create_playlist(playlist_data):
@@ -100,10 +115,10 @@ def create_playlist(playlist_data):
 
     members = get_group_members(group)
     # 2. Get tracks of each member of group
-    all_tracks = get_all_tracks(members)
-
+    all_tracks = get_all_tracks(members, pl_data["num_tracks"])
+    print(all_tracks)
     # 3. Create a list of tracks of length playlist_data.num_tracks from all the tracks for each member
-    # playlist_tracks = get_playlist_tracks(all_tracks, pl_data["num_tracks"])
+    playlist_tracks = get_playlist_tracks(all_tracks, pl_data["num_tracks"])
 
     # 4. Create playlist (empty initially)
     #   a. ENDPOINT:  https://api.spotify.com/v1/users/{user_id}/playlists
@@ -122,7 +137,9 @@ def create_playlist(playlist_data):
 
     print(create_resp)
 
-    # add_resp = add_tracks(playlist_tracks, create_resp)
+    add_resp = add_tracks(group.owner_id, playlist_tracks, create_resp)
+
+    print(add_resp)
     
     # 5. Add 3. to 4.
     #   a. ENDPOINT:  https://api.spotify.com/v1/playlists/{playlist_id}/tracks
